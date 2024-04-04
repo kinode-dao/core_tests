b9b495f58deab6e6c35dd273755d3c7efa35f54d
hosted-fornet <hosted-fornet@protonmail.com>
2024-04-04 10:44:01 -0700

    bump deps

--- chat/chat/src/lib.rs
+++ chat/chat/src/lib.rs
@@ -1,14 +1,11 @@
+use serde::{Deserialize, Serialize};
 use std::str::FromStr;
-use serde::{Serialize, Deserialize};
 
-use kinode_process_lib::{await_message, print_to_terminal, Address, Message, ProcessId, Request, Response};
+use kinode_process_lib::{await_message, call_init, println, Address, ProcessId, Request, Response};
 
 wit_bindgen::generate!({
     path: "wit",
     world: "process",
-    exports: {
-        world: Component,
-    },
 });
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,70 +22,67 @@ enum ChatResponse {
 
 type MessageArchive = Vec<(String, String)>;
 
-fn handle_message (
-    our: &Address,
-    message_archive: &mut MessageArchive,
-) -> anyhow::Result<()> {
-    let message = await_message().unwrap();
+fn handle_message(our: &Address, message_archive: &mut MessageArchive) -> anyhow::Result<()> {
+    let message = await_message()?;
 
-    match message {
-        Message::Response { .. } => {
-            print_to_terminal(0, &format!("chat: unexpected Response: {:?}", message));
-            panic!("");
-        },
-        Message::Request { ref source, ref body, .. } => {
-            match serde_json::from_slice(body)? {
-                ChatRequest::Send { ref target, ref message } => {
-                    if target == &our.node {
-                        print_to_terminal(0, &format!("chat|{}: {}", source.node, message));
-                        message_archive.push((source.node.clone(), message.clone()));
-                    } else {
-                        let _ = Request::new()
-                            .target(Address {
-                                node: target.clone(),
-                                process: ProcessId::from_str("chat:chat:template.os")?,
-                            })
-                            .body(body.clone())
-                            .send_and_await_response(5)?
-                            .unwrap();
-                    }
-                    Response::new()
-                        .body(serde_json::to_vec(&ChatResponse::Ack).unwrap())
-                        .send()
-                        .unwrap();
-                },
-                ChatRequest::History => {
-                    Response::new()
-                        .body(serde_json::to_vec(&ChatResponse::History {
-                            messages: message_archive.clone(),
-                        }).unwrap())
-                        .send()
-                        .unwrap();
-                },
+    if !message.is_request() {
+        return Err(anyhow::anyhow!("unexpected Response: {:?}", message));
+    }
+
+    let body = message.body();
+    let source = message.source();
+    match serde_json::from_slice(body)? {
+        ChatRequest::Send {
+            ref target,
+            ref message,
+        } => {
+            if target == &our.node {
+                println!("{}: {}", source.node, message);
+                message_archive.push((source.node.clone(), message.clone()));
+            } else {
+                let _ = Request::new()
+                    .target(Address {
+                        node: target.clone(),
+                        process: ProcessId::from_str(
+                            "chat:chat:template.os",
+                        )?,
+                    })
+                    .body(body)
+                    .send_and_await_response(5)?
+                    .unwrap();
             }
-        },
+            Response::new()
+                .body(serde_json::to_vec(&ChatResponse::Ack).unwrap())
+                .send()
+                .unwrap();
+        }
+        ChatRequest::History => {
+            Response::new()
+                .body(
+                    serde_json::to_vec(&ChatResponse::History {
+                        messages: message_archive.clone(),
+                    })
+                    .unwrap(),
+                )
+                .send()
+                .unwrap();
+        }
     }
     Ok(())
 }
 
-struct Component;
-impl Guest for Component {
-    fn init(our: String) {
-        print_to_terminal(0, "chat: begin");
+call_init!(init);
+fn init(our: Address) {
+    println!("begin");
 
-        let our = Address::from_str(&our).unwrap();
-        let mut message_archive: MessageArchive = Vec::new();
+    let mut message_archive: MessageArchive = Vec::new();
 
-        loop {
-            match handle_message(&our, &mut message_archive) {
-                Ok(()) => {},
-                Err(e) => {
-                    print_to_terminal(0, format!(
-                        "chat: error: {:?}",
-                        e,
-                    ).as_str());
-                },
-            };
-        }
+    loop {
+        match handle_message(&our, &mut message_archive) {
+            Ok(()) => {}
+            Err(e) => {
+                println!("error: {:?}", e);
+            }
+        };
     }
 }
